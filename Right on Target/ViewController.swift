337289e7529b0be256f0ac1f07bafd50ef58baac
ramgalin <iamgalinramil@gmail.com>
2023-08-17 19:37:44 +0200

    separate game methods and fields into a game model class

--- Right on Target/ViewController.swift
+++ Right on Target/ViewController.swift
@@ -8,69 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // guessed number
-    var number: Int = 0
-    
-    // game round
-    var round: Int = 0
-    
-    // sum of points
-    var points: Int = 0
+    var game: Game!
     
     @IBOutlet var slider: UISlider!
     @IBOutlet var label: UILabel!
-
+    
+    // MARK: Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        game = Game(startValue: 1, endValue: 50, rounds: 5)
+        updateLabelWithSecretNumber(newText: String(game.currentSecretValue))
     }
     
+    // MARK: View - Model Interaction
+    
     @IBAction func checkNumber() {
-        if round == 0 {
-            // the game just has started
-            self.number = Int.random(in: 1...50)
-            self.label.text = String(self.number)
-            self.round = 1
+        // obtain slider value
+        let numSlider = Int(self.slider.value.rounded())
+        
+        game.calculateScore(with: numSlider)
+        
+        if game.isGameEnded {
+            showAlertWith(score: game.score)
+            game.restartGame()
         } else {
-            // obtain slider value
-            let numSlider = Int(self.slider.value.rounded())
-            
-            // calculate points
-            if numSlider > self.number {
-                points += 50 - numSlider + self.number
-            } else {
-                points += 50 - self.number + numSlider
-            }
-            
-            if round == 5 {
-                // the last round
-                let alert = UIAlertController(
-                    title: "Finished",
-                    message: "You got \(self.points) points",
-                    preferredStyle: .alert
-                )
-                
-                alert.addAction(
-                    UIAlertAction(
-                        title: "Start new game",
-                        style: .default,
-                        handler: nil
-                    )
-                )
-                
-                self.present(alert, animated: true, completion: nil)
-                self.round = 1
-                self.points = 0
-            } else {
-                self.round += 1
-            }
-            
-            // generate random random and set it to label
-            self.number = Int.random(in: 1...50)
-            self.label.text = String(self.number)
+            game.startNewRound()
         }
+        
+        updateLabelWithSecretNumber(newText: String(game.currentSecretValue))
     }
-
-
+    
+    // MARK: Update View
+    // Update label text
+    private func updateLabelWithSecretNumber(newText: String) {
+        label.text = newText
+    }
+    
+    // Show alert with the score
+    private func showAlertWith(score: Int) {
+        // the last round
+        let alert = UIAlertController(
+            title: "Finished",
+            message: "You got \(score) points",
+            preferredStyle: .alert
+        )
+        
+        alert.addAction(
+            UIAlertAction(
+                title: "Start new game",
+                style: .default,
+                handler: nil
+            )
+        )
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
